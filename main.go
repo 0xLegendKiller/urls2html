5000d2e5f21884e93884a98d64dac62616fd75a3
0xlegendkiller <60841283+0xLegendKiller@users.noreply.github.com>
2025-05-23 16:20:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,14 +8,13 @@ import (
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Println("Usage: url2html <inputfile> <outputfile>")
+		fmt.Println("Usage: urls2html <inputfile> <outputfile>")
 		return
 	}
 
 	inputPath := os.Args[1]
 	outputPath := os.Args[2]
 
-	// Open the input file
 	inputFile, err := os.Open(inputPath)
 	if err != nil {
 		fmt.Println("Error opening input file:", err)
@@ -23,7 +22,6 @@ func main() {
 	}
 	defer inputFile.Close()
 
-	// Create the output file
 	outputFile, err := os.Create(outputPath)
 	if err != nil {
 		fmt.Println("Error creating output file:", err)
@@ -31,10 +29,8 @@ func main() {
 	}
 	defer outputFile.Close()
 
-	// Write HTML header
 	outputFile.WriteString("<!DOCTYPE html>\n<html>\n<head><title>URL List</title></head>\n<body>\n")
 
-	// Write each URL as a link
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
 		url := scanner.Text()
@@ -43,8 +39,6 @@ func main() {
 		}
 	}
 
-	// Write HTML footer
 	outputFile.WriteString("</body>\n</html>\n")
-
 	fmt.Printf("HTML file '%s' generated successfully.\n", outputPath)
 }
